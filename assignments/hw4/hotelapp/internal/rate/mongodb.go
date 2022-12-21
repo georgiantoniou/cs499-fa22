e7f4b555109939acee6695da7e6a8d857dbf8bdf
ganton12 <ganton12@node0.testskylake2.ucy-cs499-dc-pg0.wisc.cloudlab.us>
2022-12-21 16:01:57 -0600

    mongodb implementation ok

--- assignments/hw4/hotelapp/internal/rate/mongodb.go
+++ assignments/hw4/hotelapp/internal/rate/mongodb.go
@@ -5,6 +5,8 @@ package rate
 import (
 	log "github.com/sirupsen/logrus"
 
+	pb "github.com/ucy-coast/hotel-app/internal/rate/proto"
+
 	"github.com/ucy-coast/hotel-app/pkg/util"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -16,6 +18,16 @@ type DatabaseSession struct {
 
 func NewDatabaseSession(db_addr string) *DatabaseSession {
 	// TODO: Implement me
+
+	session, err := mgo.Dial(db_addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info("New session successfull...")
+
+	return &DatabaseSession{
+		MongoSession: session,
+	}
 }
 
 func (db *DatabaseSession) LoadDataFromJsonFile(rateJsonPath string) {
@@ -25,4 +37,21 @@ func (db *DatabaseSession) LoadDataFromJsonFile(rateJsonPath string) {
 // GetRates gets rates for hotels for specific date range.
 func (db *DatabaseSession) GetRates(hotelIds []string) (RatePlans, error) {
 	// TODO: Implement me
+	session := db.MongoSession.Copy()
+	defer session.Close()
+	c := session.DB("rate-db").C("inventory")
+
+	RatePlans := make([]*pb.RatePlan, 0)
+
+	for _, id := range hotelIds {
+		rate_plan := new(pb.RatePlan)
+		err := c.Find(bson.M{"hotelId": id}).One(&rate_plan)
+		if err != nil {
+                        continue
+			log.Fatalf("Failed get hotels data: ", err)
+		}
+		RatePlans = append(RatePlans, rate_plan)
+	}
+	return RatePlans, nil
+
 }
